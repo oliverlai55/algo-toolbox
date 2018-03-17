2534e9c6b67e837ba53b9c8cb852a4d2f04a333e
oliverlai55 <oliverlai55@gmail.com>
2018-03-16 21:51:09 -0400

    Load data correctly

--- week3/2_max_value_of_loot.js
+++ week3/2_max_value_of_loot.js
@@ -6,3 +6,23 @@ var rl = readline.createInterface({
 	terminal: false,
 });
 
+rl.on('line', readLine);
+
+let input = [];
+
+function readLine(line) {
+	if (line !== '\n') {
+		line = line.toString().split(' ');
+		input.push(line);
+	}
+
+	// number of items in bag
+	let numItems = parseInt(input[0][0]);
+
+	if (input.length === numItems + 1) {
+		let bagWeightCapacity = input[0][1];
+		let items = input.slice(1);
+		console.log(maxValueLoot(bagWeightCapacity, items));
+		process.exit();
+	}
+}
