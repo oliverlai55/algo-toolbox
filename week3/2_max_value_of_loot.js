ff1939671ab67a2292af01591e3048ec9eeb55c5
oliverlai55 <oliverlai55@gmail.com>
2018-03-17 02:01:33 -0400

    Post solution

--- week3/2_max_value_of_loot.js
+++ week3/2_max_value_of_loot.js
@@ -26,3 +26,28 @@ function readLine(line) {
 		process.exit();
 	}
 }
+
+function maxValueLoot(bagWeightCapacity, items) {
+	let finalValue = 0;
+	let index = 0;
+
+	// sort items array by the value / weight ratio
+	items.sort((a, b) => b[0] / b[1] - a[0] / a[1]);
+
+	while (bagWeightCapacity > 0 && items[index]) {
+		let currentItem = items[index];
+		let currentItemValue = parseInt(currentItem[0]);
+		let currentItemWeight = parseInt(currentItem[1]);
+
+		if (currentItemWeight < bagWeightCapacity) {
+			finalValue += currentItemValue;
+			bagWeightCapacity -= currentItemWeight;
+			index++;
+		} else {
+			finalValue += bagWeightCapacity / currentItemWeight * currentItemValue;
+			bagWeightCapacity = 0;
+		}
+	}
+
+	return finalValue.toFixed(4);
+}
