fbce5c431ca64f70aa2cc5e42a1ae622bd8c015b
Oliver Lai <oliverlai55@gmail.com>
2018-03-21 17:16:45 -0400

    Add pseudocode

--- weekly-assignments/week4/1_binary_search.js
+++ weekly-assignments/week4/1_binary_search.js
@@ -1,9 +1,30 @@
-var readline = require('readline');
+let readline = require('readline');
 
 process.stdin.setEncoding('utf8');
-var rl = readline.createInterface({
+let rl = readline.createInterface({
 	input: process.stdin,
 	terminal: false,
 });
 
 rl.on('line', readLine);
+
+let input = [];
+
+// First line 5 1 5 8 12 13
+// 5 is the length of array, the rest is sorted Array
+// Seond line 5 8 1 23 1 11
+// 5 is also the lenth, the rest are search inputs used to do binarySearch
+// put inputs in array but slice off the first input 
+
+function readLine(line) {
+  if (line !== 'n') {
+    line = line.toString().split(' ');
+    input.push(line);
+  }
+
+  if (input.length === 3) {
+
+  }
+}
+
+module.exports = binarySearch;
---
+++ weekly-assignments/week4/1_binary_search.test.js
@@ -0,0 +1,1 @@
+const binarySearch = require('./1_binary_search');
