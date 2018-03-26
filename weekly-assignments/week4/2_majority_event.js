ce4eae97eeee16476bc88af0a51c8e0738f4c8cb
oliverlai55 <oliverlai55@gmail.com>
2018-03-26 01:46:55 -0400

    Add solution

--- weekly-assignments/week4/2_majority_event.js
+++ weekly-assignments/week4/2_majority_event.js
@@ -1,7 +1,7 @@
-let readline = require('readline');
-
+const readline = require('readline');
 process.stdin.setEncoding('utf8');
-let rl = readline.createInterface({
+
+const rl = readline.createInterface({
 	input: process.stdin,
 	terminal: false,
 });
@@ -11,7 +11,7 @@ rl.on('line', readLine);
 let input = [];
 
 function readLine(line) {
-	if (line !== 'n') {
+	if (line !== '\n') {
 		line = line
 			.toString()
 			.split(' ')
@@ -21,24 +21,40 @@ function readLine(line) {
 
 	if (input.length === 2) {
 		let inputArr = input[1];
-
-		console.log(countMajority(inputArr));
+		console.log(!!countMajority(inputArr) * 1);
 		process.exit();
 	}
 }
 
 function countMajority(inputArr) {
 	if (inputArr.length === 0) return 0;
-	if (inputArr.length === 1) return [0];
+	if (inputArr.length === 1) return inputArr[0];
 
 	let midpoint = Math.floor(inputArr.length / 2);
 	let majorityVote = midpoint + 1;
-	let leftHalf = votes.slice(0, midpoint);
-	let rightHalf = votes.slice(midpoint);
+
+	let leftHalf = inputArr.slice(0, midpoint);
+	let rightHalf = inputArr.slice(midpoint);
 	let leftMajority = countMajority(leftHalf);
 	let rightMajority = countMajority(rightHalf);
+
+	if (leftMajority === rightMajority) {
+		return leftMajority;
+	}
+
+	// starts off with 0 votes
+	let leftVotes = 0;
+	let rightVotes = 0;
+
+	for (let i = 0; i < inputArr.length; i++) {
+		if (inputArr[i] === leftMajority) leftVotes++;
+		if (leftVotes >= majorityVote) return leftMajority;
+		if (inputArr[i] === rightMajority) rightVotes++;
+		if (rightVotes >= majorityVote) return rightMajority;
+	}
+	return 0;
 }
 
-module.exports = {
-	countMajority,
-};
+// module.exports = {
+// 	countMajority,
+// };
--- weekly-assignments/week4/old_2_majority_event.js
+++ weekly-assignments/week4/old_2_majority_event.js
@@ -58,3 +58,62 @@ function countMajority(arrSize, inputArr) {
 module.exports = {
 	countMajority,
 };
+
+let readline = require('readline');
+
+process.stdin.setEncoding('utf8');
+let rl = readline.createInterface({
+	input: process.stdin,
+	terminal: false,
+});
+
+rl.on('line', readLine);
+
+let input = [];
+
+function readLine(line) {
+	if (line !== 'n') {
+		line = line
+			.toString()
+			.split(' ')
+			.map(num => parseInt(num));
+		input.push(line);
+	}
+
+	if (input.length === 2) {
+		let inputArr = input[1];
+
+		console.log(!!countMajority(inputArr) * 1);
+		process.exit();
+	}
+}
+
+function countMajority(inputArr) {
+	if (inputArr.length === 0) return 0;
+	if (inputArr.length === 1) return inputArr[0];
+
+	let midpoint = Math.floor(inputArr.length / 2);
+	let majorityVote = midpoint + 1;
+	let leftHalf = inputArr.slice(0, midpoint);
+	let rightHalf = inputArr.slice(midpoint);
+	let leftMajority = countMajority(leftHalf);
+	let rightMajority = countMajority(rightHalf);
+
+	if (leftMajority === rightMajority) {
+		return leftMajority;
+	}
+
+	for (let i = 0; i < inputArr.length; i++) {
+		let leftVotes = 0;
+		let rightVotes = 0;
+		if (inputArr[i] === leftMajority) leftVotes++;
+		if (leftVotes >= majorityVote) return leftMajority;
+		if (inputArr[i] === rightMajority) rightVotes++;
+		if (rightVotes >= majorityVote) return rightMajority;
+	}
+	return 0;
+}
+
+// module.exports = {
+// 	countMajority,
+// };
