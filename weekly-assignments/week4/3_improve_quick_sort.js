2e0c7d3ba323c905cc38648054124e171655375b
Oliver Lai <oliverlai55@gmail.com>
2018-03-28 14:13:56 -0400

    Add files

--- weekly-assignments/week4/3_improve_quick_sort.js
+++ weekly-assignments/week4/3_improve_quick_sort.js
@@ -2,8 +2,8 @@ const readline = require('readline');
 process.stdin.setEncoding('utf8');
 
 const rl = readline.createInterface({
-	input: process.stdin,
-	terminal: false,
+  input: process.stdin,
+  terminal: false
 });
 
 let input = [];
@@ -11,56 +11,49 @@ let input = [];
 rl.on('line', readLine);
 
 function readLine(line) {
-	if (line !== '\n') {
-		input.push(
-			line
-				.toString()
-				.split(' ')
-				.map(num => parseInt(num)),
-		);
-	}
+  if (line !== "\n") {
+    input.push(line.toString().split(' ').map(num => parseInt(num)));
+  }
 
-	if (input.length === 2) {
-		let nums = input[1];
-		quickSort(nums);
-		nums.forEach(num => process.stdout.write(`${num} `));
-		process.exit();
-	}
+  if (input.length === 2) {
+    let nums = input[1];
+    quickSort(nums);
+    nums.forEach(num => process.stdout.write(`${num} `));
+    process.exit();
+  }
 }
 
 const quickSort = (nums, start = 0, end = nums.length - 1) => {
-	if (end <= start) return;
-	let randomElement = start + Math.floor(Math.random() * (end - start + 1));
-	swap(nums, start, randomElement);
-	let [m1, m2] = partition3(nums, start, end);
-	// console.log([m1, m2]);
-	// console.log('m1', m1);
-	// console.log('m2', m2);
-	quickSort(nums, start, m1 - 1);
-	quickSort(nums, m2 + 1, end);
-};
+  if (end <= start) return;
+  let randomElement = start + Math.floor(Math.random() * (end - start + 1));
+  swap(nums, start, randomElement);
+  let [m1, m2] = partition3(nums, start, end);
+  quickSort(nums, start, m1 - 1);
+  quickSort(nums, m2 + 1, end);
+}
 
 const swap = (array, a, b) => {
-	let temp = array[a];
-	array[a] = array[b];
-	array[b] = temp;
-};
+  let temp = array[a];
+  array[a] = array[b];
+  array[b] = temp;
+}
 
 const partition3 = (array, start, end) => {
-	let less = (equal = start);
-	let target = array[start];
+// we set equal to start of the array? Why?
+  let less = equal = start; // What exactly is 'less' ?
+  let target = array[start];  // picking 1st element as pivot?
 
-	for (let i = start + 1; i <= end; i++) {
-		if (array[i] < target) {
-			less++;
-			equal++;
-			swap(array, i, less);
-			if (less !== equal) swap(array, i, equal);
-		} else if (array[i] === target) {
-			equal++;
-			swap(array, i, equal);
-		}
-	}
-	swap(array, less, start);
-	return [less, equal];
-};
+  for (let i = start + 1; i <= end; i++) {
+    if (array[i] < target) { // if array[i] < pivot, swap i with the pivot index?
+      less++;
+      equal++;
+      swap(array, i, less);
+      if (less !== equal) swap(array, i, equal);
+    } else if (array[i] === target) {
+      equal++;
+      swap(array, i, equal);
+    }
+  }
+  swap(array, less, start);
+  return [less, equal];
+}
---
+++ weekly-assignments/week4/old_3_improve_quick_sort.js
@@ -0,0 +1,66 @@
+const readline = require('readline');
+process.stdin.setEncoding('utf8');
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	terminal: false,
+});
+
+let input = [];
+
+rl.on('line', readLine);
+
+function readLine(line) {
+	if (line !== '\n') {
+		input.push(
+			line
+				.toString()
+				.split(' ')
+				.map(num => parseInt(num)),
+		);
+	}
+
+	if (input.length === 2) {
+    let nums = input[1];
+    quickSort(nums);
+    nums.forEach(num => process.stdout.write(`${num} `));
+    process.exit();
+  }
+}
+
+const quickSort = (nums, start = 0, end = nums.length - 1) => {
+	if (end <= start) return;
+	let randomElement = start + Math.floor(Math.random() * (end - start + 1));
+	swap(nums, start, randomElement);
+	let [m1, m2] = partition3(nums, start, end);
+
+	quickSort(nums, start, m1 - 1);
+	quickSort(nums, m2 + 1, end);
+};
+
+const swap = (array, a, b) => {
+	let temp = array[a];
+	array[a] = array[b];
+	array[b] = temp;
+};
+
+
+const partition3 = (array, start, end) => {
+	let equal = start; // we set equal to start of the array (what does that mean?)
+	let less = equal;
+	let target = array[start]; // picking 1st element as pivot?
+
+	for (let i = start + 1; i <= end; i++) {
+		if (array[i] < target) { 
+			less++;
+			equal++;
+			swap(array, i, less);
+			if (less !== equal) swap(array, i, equal);
+		} else if (array[i] === target) {
+			equal++;
+			swap(array, i, equal);
+		}
+	}
+	swap(array, less, start);
+	return [less, equal];
+};
