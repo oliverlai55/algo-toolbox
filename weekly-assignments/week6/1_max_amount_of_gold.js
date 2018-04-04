d9572f5ea277057824afde589163070f5bc73060
Oliver Lai <oliverlai55@gmail.com>
2018-04-04 18:51:09 -0400

    Add changes?

--- weekly-assignments/week6/1_max_amount_of_gold.js
+++ weekly-assignments/week6/1_max_amount_of_gold.js
@@ -1,5 +1,21 @@
 const readline = require('readline');
+process.stdin.setEncoding('utf8');
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  terminal: false,
+});
+
+rl.on('line', readLine);
+
+let input = [];
+
+function readLine(line) {
+  if (line !== 'n') {
+    line = line.toString().split(' ');
+    input.push(line);''
+  }
+}
 // use memoization
 // ask bryan why if val > value(w) value(w) = val;?
 // What is val exactly?
